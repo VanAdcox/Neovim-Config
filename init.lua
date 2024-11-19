fbbe29c90fe147cac9461b77430ee3479fbf4ce5
Your Name <you@example.com>
2024-11-18 22:09:25 -0600

    telescope binds

--- init.lua
+++ init.lua
@@ -1,8 +1,17 @@
 require("config.lazy")
---require("options") -- the docs say not to manually require this but it wont work if i dont :shrug:
+
+-- Vim Bindings / Options
 vim.wo.relativenumber = true
+
+
+
 vim.api.nvim_set_keymap('n', '-', ':Oil parent<CR>', {noremap = true})
 
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
 -- plugin stuff
 require("mason").setup()
